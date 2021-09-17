da55bdb3cb91c2fadc469d183d5a4c9666e9c4c8
JoelmaBittencourt <joohtrader@gmail.com>
2021-09-16 22:00:54 -0300

    atualizaçao

--- src/main/java/br/com/forum/modelo/Curso.java
+++ src/main/java/br/com/forum/modelo/Curso.java
@@ -4,16 +4,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 @Entity
 public class Curso {
 
-	@Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String categoria;
 	
-	
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
